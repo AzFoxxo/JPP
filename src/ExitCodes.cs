92cded41c5f811254b785cae11c45521868642c6
Az Foxxo <az@azfoxxo.com>
2026-01-20 09:46:35 +0000

    Added basic exit codes

---
+++ src/ExitCodes.cs
@@ -0,0 +1,17 @@
+﻿/*
+*   Java++ (JPP) - Java superset transpiler written in C#
+*   Copyright (C) 2026 Az Foxxo
+*   See LICENSE for details.
+*   ----------
+*   Exit codes for Java++ transpiler.
+*/
+namespace JPP;
+
+public enum ExitCodes
+{
+    Success = 0,
+    StandardError = 1,
+    InvalidArguments = 2,
+    FileNotFound = 3,
+    ParseError = 4,
+}
--- src/Program.cs
+++ src/Program.cs
@@ -2,13 +2,27 @@
 *   Java++ (JPP) - Java superset transpiler written in C#
 *   Copyright (C) 2026 Az Foxxo
 *   See LICENSE for details.
+*   ----------
+*   Entry point and argument parsing for Java++ transpiler.
+*
+*   Arguments:
+*       -i, --input <file>       Input Java++ file path (required or use of project flag is)
+*       -o, --output <file>      Output Java file path (optional, defaults to displaying the output to the console)
+*       -p, --project <file>     Input Java++ project file path (directory to recursively transpile all .jpp files) (required or use of input flag is)
+*       -h, --help               Display help information
+*       -v, --version            Display version information
 */
+
 namespace JPP;
 
 public class Program
 {
+    public const string Version = "0.0.0 dev";
     public static int Main(string[] args)
     {
-        return 0;
+        // Print the version for debugging purposes
+        Console.WriteLine($"Java++ {Version} version.");
+
+        return (int)ExitCodes.Success;
     }
 }
